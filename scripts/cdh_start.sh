5282f90e737a948b8c5f8985626ab7c92b86b53b
needmorenodes <brian@needmorenodes.com>
2018-07-10 12:52:35 -0400

    Added onfigs to the CDH start to allow for streamsets to write to hdfs

--- scripts/cdh_start.sh
+++ scripts/cdh_start.sh
@@ -1,1 +1,3 @@
-docker run --hostname=quickstart.cloudera --privileged=true -t -i -p 8888:8888 -p 80:80 -p 7180:7180 needmorenodes/clusterdock:5.13 /usr/bin/docker-quickstart
+#!/bin/bash
+
+docker run --name quickstart.cloudera --network mynet --hostname=quickstart.cloudera --privileged=true -t -i -p 50070:50070 -p 50075:50075 -p 8020:8020 -p 8888:8888 -p 80:80 -p 7180:7180 needmorenodes/clusterdock:5.13 /usr/bin/docker-quickstart
