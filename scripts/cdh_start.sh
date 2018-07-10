ee0e6b8160248de9a70e3d013339ac038cb1cc94
needmorenodes <brian@needmorenodes.com>
2018-07-10 13:11:52 -0400

    Added logic to cloudera start

--- scripts/cdh_start.sh
+++ scripts/cdh_start.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-docker run --name quickstart.cloudera --network mynet --hostname=quickstart.cloudera --privileged=true -t -i -p 50070:50070 -p 50075:50075 -p 8020:8020 -p 8888:8888 -p 80:80 -p 7180:7180 needmorenodes/clusterdock:5.13 /usr/bin/docker-quickstart
+if [ "$(docker ps -a | grep quickstart.cloudera)" ]; then
+  docker start quickstart.cloudera
+else
+  docker run --name quickstart.cloudera \
+  --network mynet \
+  --hostname=quickstart.cloudera \
+  --privileged=true -t -d \
+  -p 50070:50070 -p 50075:50075 -p 8020:8020 -p 8888:8888 -p 80:80 -p 7180:7180 \
+  needmorenodes/clusterdock:5.13 /usr/bin/docker-quickstart
+fi
