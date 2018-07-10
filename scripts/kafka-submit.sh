0daa779bd56ba231b3e2c11596f08a36863a880d
needmorenodes <brian@needmorenodes.com>
2018-07-10 10:47:50 -0400

    Utilized logging in the docker container

--- scripts/kafka-submit.sh
+++ scripts/kafka-submit.sh
@@ -5,9 +5,9 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # Set the network device in the conf.sh file that kafkas IP will bind to
 source ${DIR}/../conf/conf.sh
 
-docker run -it --rm -v $DIR/../target/scala-2.11/:/jars/ --name spark \
+docker run -it --rm -v $DIR/../target/scala-2.11/:/jars/ -v $DIR/logs:/logs/ --name spark \
     --env SPARK_PACKAGES=org.apache.spark:spark-sql-kafka-0-10_2.11:2.2.0 \
     --env SPARK_CLASS=spark.kafka.examples.KafkaStreaming \
     --env SPARK_JAR=KafkaStreaming-assembly-0.1.jar \
     --env SPARK_PARAMS="-bootstrap_servers `ipconfig getifaddr ${NETWORK_DEVICE}`:9092 -input_topic test -output_topic testout" \
-    needmorenodes/spark-submit-local:2.2
+    needmorenodes/spark-submit-local:2.1.1
