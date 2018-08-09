e751f07d4130e04bef7b005f2f0d5c38742528b5
needmorenodes <brian@needmorenodes.com>
2018-08-09 15:55:37 -0400

    Fixed jar name

--- scripts/kafka-submit.sh
+++ scripts/kafka-submit.sh
@@ -5,6 +5,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 docker run -it --rm --network mynet -v $DIR/../target/scala-2.11/:/jars/ -v $DIR/logs:/logs/ --name spark \
     --env SPARK_PACKAGES=org.apache.spark:spark-sql-kafka-0-10_2.11:2.2.0 \
     --env SPARK_CLASS=spark.kafka.examples.KafkaStreaming \
-    --env SPARK_JAR=KafkaStreaming-assembly-0.1.jar \
+    --env SPARK_JAR=Spark-Streaming-Examples-assembly-0.1.jar \
     --env SPARK_PARAMS="-bootstrap_servers kafka:9092 -input_topic test -output_topic testout" \
     needmorenodes/spark-submit-local:2.2.0
